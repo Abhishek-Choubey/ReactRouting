658da1a63c43ff0cfdae1bb69754c3577b73541b
Abhishek-Choubey <96122002+Abhishek-Choubey@users.noreply.github.com>
2022-03-21 02:54:06 +0530

    committed

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export const HomePage = () => {
   return (
     <>
-    <h1>Home Page</h1>
+    <h1>Home</h1>
       <div
         style={{
           display: "flex",
